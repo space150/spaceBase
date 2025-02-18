daaef305b36c9a542bda7a885e946a4a9825371a
Bob Kelly <bob.kelly@space150.com>
2025-02-18 15:06:12 -0600

    removed next config sample; deprecation silencing not needed

--- next.config.sample.ts
+++
@@ -1,9 +0,0 @@
-// import type { NextConfig } from 'next'
-
-// const nextConfig: NextConfig = {
-//   sassOptions: {
-//     silenceDeprecations: ['legacy-js-api'],
-//   },
-// }
-
-// export default nextConfig
